6ca8678e8a8ab5e823d35c660fbbd107ac6dc1b6
Javid Asgarov <asgarov1@gmail.com>
2020-03-08 19:28:19 +0100

    implemeted search orders function for admin account page

--- src/main/java/com/asgarov/shop/controller/AdminController.java
+++ src/main/java/com/asgarov/shop/controller/AdminController.java
@@ -2,6 +2,7 @@ package com.asgarov.shop.controller;
 
 import javax.servlet.http.HttpServletRequest;
 
+import com.asgarov.shop.service.OrderService;
 import com.asgarov.shop.service.UserService;
 
 import org.springframework.stereotype.Controller;
@@ -13,24 +14,39 @@ import org.springframework.web.bind.annotation.RequestParam;
 @Controller
 public class AdminController {
     private UserService userService;
+    private OrderService orderService;
 
-    public AdminController(final UserService userService) {
+    public AdminController(final UserService userService, final OrderService orderService) {
         this.userService = userService;
+        this.orderService = orderService;
     }
 
     @GetMapping("/admin")
     public String admin() {
-        return "userPages/admin/index";
+        return "admin/index";
     }
 
     @PostMapping("/searchUsersBy")
-    public String searchUsersBy(@RequestParam String searchParameter, @RequestParam String searchOption, HttpServletRequest request, Model model) {
+    public String searchUsersBy(@RequestParam String searchParameter, @RequestParam String searchOption,
+           Model model) {
         switch (searchOption.toLowerCase()){
-            case "surname": model.addAttribute("results", userService.findAllByLastName(searchParameter)); break;
-            case "email": model.addAttribute("results", userService.findAllByEmail(searchParameter)); break;
-            case "phone": model.addAttribute("results", userService.findAllByPhone(searchParameter)); break;
+            case "surname": model.addAttribute("userResults", userService.findAllByLastName(searchParameter)); break;
+            case "email": model.addAttribute("userResults", userService.findAllByEmail(searchParameter)); break;
+            case "phone": model.addAttribute("userResults", userService.findAllByPhone(searchParameter)); break;
+            default: model.addAttribute("userResults", userService.findAllByLastNamePhoneEmail(searchParameter)); break;
         }
-        return "userPages/admin/index";
+        return "admin/index";
+    }
+
+    @PostMapping("/searchOrdersBy")
+    public String searchOrdersBy(@RequestParam String searchParameter, @RequestParam String searchOption, Model model) {
+        switch (searchOption.toLowerCase()){
+        case "id": model.addAttribute("orders", orderService.findAllById(searchParameter)); break;
+        case "date": model.addAttribute("orders", orderService.findAllByDate(searchParameter)); break;
+        case "surname": model.addAttribute("orders", orderService.findAllByLastName(searchParameter)); break;
+        default: model.addAttribute("orders", orderService.findAllByIdDateLastName(searchParameter)); break;
+        }
+        return "admin/index";
     }
 
 }
